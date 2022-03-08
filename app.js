e00b15031114c9e98f6ec494334523842c8e7044
Godfred Nsabo <72404654+godfred-nsabo@users.noreply.github.com>
2022-03-08 10:56:09 +0000

    feat: read env files

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=> {
+require('dotenv/config');
+
+const api = process.env.API_URL;
+// http://localhost:3000/api/v1/products
+app.get(api+'/products', (req, res)=> {
   res.send('My API')
 });
 app.listen(3000, () => {
+  console.log(api);
   console.log('server is running on http://localhost:3000')
 })
