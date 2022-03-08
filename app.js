b86121b127593f402e6ccbc19acf799e5b1af83c
Godfred Nsabo <72404654+godfred-nsabo@users.noreply.github.com>
2022-03-08 17:59:45 +0000

    feat: connecting to databse

--- app.js
+++ app.js
@@ -3,16 +3,24 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-// const bodyParser = require('body-parser');
-// const use = require('./backend/middlewares/appMiddleware')
+const mongoose = require('mongoose');
+const { options } = require('nodemon/lib/config');
+
+
 require('dotenv/config');
 
 /** middlewares */
 // app.use(bodyParser.json())
+
+
 app.use(express.json());
 app.use(morgan('tiny'));
+
+
 const api = process.env.API_URL;
+
 // http://localhost:3000/api/v1/products
+
 app.get(`${api}/products`, (req, res)=> {
   const product = {
     id: 1,
@@ -27,6 +35,17 @@ app.post(`${api}/products`, (req, res)=> {
 	console.log(newProduct);
 	res.send(newProduct)
   });
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+  dbName: 'shop-Commerce-database'
+})
+.then(()=> {
+  console.log('Database Connection is ready...')
+})
+.catch((err) => {
+  console.log(err);
+})
+
 app.listen(3000, () => {
  
   console.log('server is running on http://localhost:3000')
