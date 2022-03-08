30dd178658350e8c258d6ccb82f3c1f4707de81f
Godfred Nsabo <72404654+godfred-nsabo@users.noreply.github.com>
2022-03-08 17:27:22 +0000

    feat: logging API requests

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
+'use strict';
+
 const express = require('express');
 const app = express();
-
+const morgan = require('morgan');
+// const bodyParser = require('body-parser');
+// const use = require('./backend/middlewares/appMiddleware')
 require('dotenv/config');
 
+/** middlewares */
+// app.use(bodyParser.json())
+app.use(express.json());
+app.use(morgan('tiny'));
 const api = process.env.API_URL;
 // http://localhost:3000/api/v1/products
-app.get(api+'/products', (req, res)=> {
-  res.send('My API')
+app.get(`${api}/products`, (req, res)=> {
+  const product = {
+    id: 1,
+    name: 'hair dresser',
+    image: 'some_url'
+  }
+  res.send(product)
 });
+
+app.post(`${api}/products`, (req, res)=> {
+	const newProduct = req.body;
+	console.log(newProduct);
+	res.send(newProduct)
+  });
 app.listen(3000, () => {
-  console.log(api);
+ 
   console.log('server is running on http://localhost:3000')
 })
