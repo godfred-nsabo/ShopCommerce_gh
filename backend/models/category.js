dd1f9e4fd852d119e10441eacacd57627ceed1df
Godfred Nsabo <72404654+godfred-nsabo@users.noreply.github.com>
2022-03-09 14:20:43 +0000

    feat: categories schema

--- backend/models/category.js
+++ backend/models/category.js
@@ -1,7 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const categorySchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    color: {
+        type: String,
+    },
+    icon: {
+        type: String,
+    },
+    image: {
+        type: String,
+        default: "",
+    },
+});
 
-})
-
-exports.Category = mongoose.model('Category', categorySchema);
+exports.Category = mongoose.model("Category", categorySchema);
