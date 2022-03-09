7e1fe670c02d4af8a20947387c37d88c8d27cc26
Godfred Nsabo <72404654+godfred-nsabo@users.noreply.github.com>
2022-03-09 14:23:03 +0000

    refactor: updated files

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const cors = require('cors')
-require('dotenv/config');
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
 
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 
 /** Middleware */
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
-
- /** Routes */
-const productRoutes = require('./backend/routes/product')
+/** Routes */
+const productRoutes = require("./backend/routes/product");
 const categoriesRoutes = require("./backend/routes/categories");
 const usersRoutes = require("./backend/routes/users");
 const ordersRoutes = require("./backend/routes/orders");
@@ -28,22 +27,20 @@ app.use(`${api}/products`, productRoutes);
 app.use(`${api}/users`, usersRoutes);
 app.use(`${api}/orders`, ordersRoutes);
 
-
 /** Database */
 mongoose
-.connect(process.env.CONNECTION_STRING, {
-  dbName: 'shop-Commerce-database'
-})
-.then(()=> {
-  console.log('Database Connection is ready...')
-})
-.catch((err) => {
-  console.log(err);
-})
+  .connect(process.env.CONNECTION_STRING, {
+    dbName: "shop-Commerce-database",
+  })
+  .then(() => {
+    console.log("Database Connection is ready...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 /** Server */
 
 app.listen(3000, () => {
- 
-  console.log('server is running on http://localhost:3000')
-})
+  console.log("server is running on http://localhost:3000");
+});
