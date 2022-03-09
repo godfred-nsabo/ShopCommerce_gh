110ec1c13ce4b65aa1f5bf1d6361bb884cef916c
Godfred Nsabo <72404654+godfred-nsabo@users.noreply.github.com>
2022-03-09 14:18:42 +0000

    feat: product schema

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,26 +1,67 @@
-'use strict';
+"use strict";
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: {
-    type: Number,
-    required: true
-  }
-})
+    id: {
+        type: mongoose.Schema.Types.ObjectId,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    richDescription: {
+        type: String,
+        default: "",
+    },
+    image: {
+        type: String,
+        default: "",
+    },
+    images: [
+        {
+            type: String,
+        },
+    ],
+    brand: {
+        type: String,
+        default: "",
+    },
+    price: {
+        type: Number,
+        default: 0,
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true,
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 500,
+    },
+    rating: {
+        type: Number,
+        default: 0,
+    },
+    numReviews: {
+      type: Number,
+      default: 0,
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now,
+    },
+});
 
-exports.Product = mongoose.model('Product', productSchema);
-
-const eProducts = mongoose.Schema({
-  id: String,
-  name: String,
-  description: String,
-  richDescription: String,
-  image: String,
-  images: String,
-
-})
-
-exports.Products = mongoose.model('ListedProduct', eProducts);
+exports.Product = mongoose.model("Product", productSchema);
