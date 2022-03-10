48eaff41e40c9fc0d12c0643af3b21a644cafa19
Godfred Nsabo <72404654+godfred-nsabo@users.noreply.github.com>
2022-03-10 10:55:46 +0000

    feat: changing "_id" to "id" and deleting "__v" for user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -9,4 +9,21 @@ const userSchema = mongoose.Schema({
     }
 })
 
+/** Duplicate the ID field */
+userSchema.virtual("id").get(function () {
+    return this._id.toHexString();
+});
+/** Ensure virtual fields are serialised. */
+userSchema.set("toJSON", {
+    virtuals: true,
+    versionKey: false,
+
+    /**remove the _id of every document before returning the result */
+    transform: function (doc, ret, otpions) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+    },
+});
+
 exports.User = mongoose.model('User', userSchema);
