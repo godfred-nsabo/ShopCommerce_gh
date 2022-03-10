d8affc923faaa5363e10cd1cb15a7a873658c498
Godfred Nsabo <72404654+godfred-nsabo@users.noreply.github.com>
2022-03-10 10:55:05 +0000

    feat: changing "_id" to "id" and deleting "__v" for category schema

--- backend/models/category.js
+++ backend/models/category.js
@@ -17,4 +17,21 @@ const categorySchema = mongoose.Schema({
     },
 });
 
+/** Duplicate the ID field */
+categorySchema.virtual("id").get(function () {
+    return this._id.toHexString();
+});
+/** Ensure virtual fields are serialised. */
+categorySchema.set("toJSON", {
+    virtuals: true,
+    versionKey: false,
+
+    /**remove the _id of every document before returning the result */
+    transform: function (doc, ret, otpions) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+    },
+});
+
 exports.Category = mongoose.model("Category", categorySchema);
