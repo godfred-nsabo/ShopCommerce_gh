52891adcb6b4894db38a12ab167a52ab24ca0437
Godfred Nsabo <72404654+godfred-nsabo@users.noreply.github.com>
2022-03-10 10:55:23 +0000

    feat: changing "_id" to "id" and deleting "__v" for order schema

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,7 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const orderSchema = mongoose.Schema({
+const orderSchema = mongoose.Schema({});
 
-})
+/** Duplicate the ID field */
+orderSchema.virtual("id").get(function () {
+    return this._id.toHexString();
+});
+/** Ensure virtual fields are serialised. */
+orderSchema.set("toJSON", {
+    virtuals: true,
+    versionKey: false,
 
-exports.Order = mongoose.model('Order', orderSchema);
+    /**remove the _id of every document before returning the result */
+    transform: function (doc, ret, otpions) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+    },
+});
+
+exports.Order = mongoose.model("Order", orderSchema);
