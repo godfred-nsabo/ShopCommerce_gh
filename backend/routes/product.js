26bd94e21af8828fdb8b91b62a37a6d1f70b8bb0
Godfred Nsabo <72404654+godfred-nsabo@users.noreply.github.com>
2022-03-10 09:31:15 +0000

    feat: getting featured products routes

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -4,7 +4,14 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../controllers/product");
 
-router.get("/", Product.getProduct);
 router.post("/", Product.addProduct);
+router.get("/", Product.getProduct);
+router.get("/get/stats", Product.getStatistics);
+
+router.get("/get/featured/:count", Product.getFeaturedPrdt);
+
+router.get("/:id", Product.getProductById);
+router.put("/:id", Product.updateProduct);
+router.delete("/:id", Product.deleteProduct);
 
 module.exports = router;
