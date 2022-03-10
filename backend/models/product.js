8a8bba6904df33592d8315694f5f175ea22b04aa
Godfred Nsabo <72404654+godfred-nsabo@users.noreply.github.com>
2022-03-10 10:55:35 +0000

    feat: changing "_id" to "id" and deleting "__v" for product schema

--- backend/models/product.js
+++ backend/models/product.js
@@ -3,9 +3,9 @@
 const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
-    id: {
-        type: mongoose.Schema.Types.ObjectId,
-    },
+    // id: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    // },
     name: {
         type: String,
         required: true,
@@ -37,7 +37,7 @@ const productSchema = mongoose.Schema({
     },
     category: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category',
+        ref: "Category",
         required: true,
     },
     countInStock: {
@@ -51,8 +51,8 @@ const productSchema = mongoose.Schema({
         default: 0,
     },
     numReviews: {
-      type: Number,
-      default: 0,
+        type: Number,
+        default: 0,
     },
     isFeatured: {
         type: Boolean,
@@ -61,6 +61,22 @@ const productSchema = mongoose.Schema({
     dateCreated: {
         type: Date,
         default: Date.now,
+    },
+});
+/** Duplicate the ID field */
+productSchema.virtual("id").get(function () {
+    return this._id.toHexString();
+});
+/** Ensure virtual fields are serialised. */
+productSchema.set("toJSON", {
+    virtuals: true,
+    versionKey: false,
+
+    /**remove the _id of every document before returning the result */
+    transform: function (doc, ret, otpions) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
     },
 });
 
