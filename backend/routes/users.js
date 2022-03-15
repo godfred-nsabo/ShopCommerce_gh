23f5a865f63bd7c87fb49ac625deb9ba20efa099
Godfred Nsabo <72404654+godfred-nsabo@users.noreply.github.com>
2022-03-15 15:04:04 +0000

    feat: added user login routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,6 +5,7 @@ const Route = express.Router();
 Route.get("/", User.getUser);
 Route.post("/", User.addUser);
 
+Route.post("/login", User.userLogin);
 Route.get("/:id", User.getSingleUser);
 
 module.exports = Route;
